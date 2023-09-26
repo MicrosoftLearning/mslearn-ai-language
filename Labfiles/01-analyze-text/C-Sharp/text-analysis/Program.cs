8fea5e1701b88b6a6db1a0c891459eeb49eb1e37
Ivor Berry <ivorb@users.noreply.github.com>
2023-09-26 16:15:57 -0400

    initial commit of labs

---
+++ Labfiles/01-analyze-text/C-Sharp/text-analysis/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+// Import namespaces
+
+
+namespace text_analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Get config settings from AppSettings
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                string cogSvcEndpoint = configuration["CognitiveServicesEndpoint"];
+                string cogSvcKey = configuration["CognitiveServiceKey"];
+
+                // Set console encoding to unicode
+                Console.InputEncoding = Encoding.Unicode;
+                Console.OutputEncoding = Encoding.Unicode;
+
+                // Create client using endpoint and key
+
+
+                // Analyze each text file in the reviews folder
+                var folderPath = Path.GetFullPath("./reviews");  
+                DirectoryInfo folder = new DirectoryInfo(folderPath);
+                foreach (var file in folder.GetFiles("*.txt"))
+                {
+                    // Read the file contents
+                    Console.WriteLine("\n-------------\n" + file.Name);
+                    StreamReader sr = file.OpenText();
+                    var text = sr.ReadToEnd();
+                    sr.Close();
+                    Console.WriteLine("\n" + text);
+
+                    // Get language
+
+
+                    // Get sentiment
+
+
+                    // Get key phrases
+
+
+                    // Get entities
+
+
+                    // Get linked entities
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+
+    }
+}
---
+++ Labfiles/04-text-classification/classify-text.ps1
@@ -0,0 +1,96 @@
+# Update these with your service and model values
+$key="<YOUR-KEY>"
+$endpoint="<YOUR-ENDPOINT>"
+$projectName = "ClassifyLab"
+$deploymentName = "articles"
+$verbose = $true
+
+# Set up headers for API call
+$headers = @{}
+$headers.add("Ocp-Apim-Subscription-Key", $key)
+$headers.add("Content-Type","application/json")
+
+# Get text to classify
+$text_file = "test1.txt"
+if ($args.count -gt 0)
+{
+    $text_file = $args[0]
+}
+
+try {
+    $contents = get-content .\$text_file -raw -ErrorAction Stop
+}
+catch {
+    Write-Host "`nError reading provided file, please verify file exists`n"
+    Exit
+}
+
+# Build body of for API call
+$data = @{
+    "tasks" = @{
+        "customClassificationTasks" = @(
+            @{
+                "parameters"= @{
+                      "project-name" = $projectName
+                      "deployment-name" = $deploymentName
+                }
+            }
+        )
+    }
+    "analysisInput" = @{
+        "documents" = @(
+            @{
+                "id" = "document_singleClassification"
+                "text" = $contents
+            }
+        )
+    }
+} | ConvertTo-Json -Depth 2
+
+# Post text for classification
+Write-Host("`n***Submitting text classification task***")
+$response = Invoke-WebRequest -Method Post `
+          -Uri "$($endpoint)text/analytics/v3.2-preview.2/analyze" `
+          -Headers $headers `
+          -Body $data
+
+# Output response if desired
+if ($verbose) {
+    Write-Host("`nResponse header:$($response.Headers['Operation-Location'])`n")
+}
+
+# Extract the URL from the response
+# to call the API to getting the analysis results
+$resultUrl = $($response.Headers['Operation-Location'])
+
+# Create the header for the REST GET with only the subscription key
+$resultHeaders = @{}
+$resultHeaders.Add( "Ocp-Apim-Subscription-Key", $key )
+
+# Get the results
+# Continue to request results until the analysis is "succeeded"
+Write-Host "Getting results..."
+Do {
+    $result = Invoke-RestMethod -Method Get `
+            -Uri $resultUrl `
+            -Headers $resultHeaders | ConvertTo-Json -Depth 10
+
+    $analysis = ($result | ConvertFrom-Json)
+} while ($analysis.status -ne "succeeded")
+Write-Host "...Done`n"
+
+# Access the relevant fields from the analysis
+$classification = $result | ConvertFrom-Json
+$docs = $classification.tasks.customSingleClassificationTasks[0].results.documents
+
+# Output response if desired
+if ($verbose) {
+    Write-Host("GET JSON Response:`n$result`n")
+}
+
+for (($idx = 0); $idx -lt $docs.Length; $idx++) {
+    $item = $docs[$idx] 
+    Write-Host ("Document #", ($idx+1))
+    Write-Host ("  - Predicted Category: ", $($item.classification.category))
+    Write-Host ("  - Confidence: ",$($item.classification.confidenceScore))
+}
