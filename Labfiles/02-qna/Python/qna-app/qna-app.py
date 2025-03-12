d37ef03d46d6841c824d32edf9f1119dfb3919a8
UncleJ3ff <92350671+UncleJ3ff@users.noreply.github.com>
2025-03-12 12:47:02 -0700

    Update qna-app.py

--- Labfiles/02-qna/Python/qna-app/qna-app.py
+++ Labfiles/02-qna/Python/qna-app/qna-app.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 import os
 
-# Import namespaces
+# import namespaces
+ from azure.core.credentials import AzureKeyCredential
+ from azure.ai.language.questionanswering import QuestionAnsweringClientImport namespaces
 
 
 def main():
@@ -13,10 +15,24 @@ def main():
         ai_project_name = os.getenv('QA_PROJECT_NAME')
         ai_deployment_name = os.getenv('QA_DEPLOYMENT_NAME')
 
-        # Create client using endpoint and key
+         # Create client using endpoint and key
+ credential = AzureKeyCredential(ai_key)
+ ai_client = QuestionAnsweringClient(endpoint=ai_endpoint, credential=credential)
 
 
-        # Submit a question and display the answer
+         # Submit a question and display the answer
+ user_question = ''
+ while True:
+     user_question = input('\nQuestion:\n')
+     if user_question.lower() == "quit":                
+         break
+     response = ai_client.get_answers(question=user_question,
+                                     project_name=ai_project_name,
+                                     deployment_name=ai_deployment_name)
+     for candidate in response.answers:
+         print(candidate.answer)
+         print("Confidence: {}".format(candidate.confidence))
+         print("Source: {}".format(candidate.source))
 
 
 
