d49c6c6a57b27962d5a529b03d7060ea24e993ad
Yasharjs <yasharjs@gmail.com>
2025-10-02 15:55:24 -0400

    Update .gitignore and enhance qna-app.py for environment configuration and answer retrieval

--- Labfiles/02-qna/Python/qna-app/qna-app.py
+++ Labfiles/02-qna/Python/qna-app/qna-app.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
-
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.questionanswering import QuestionAnsweringClient
 
 def main():
     try:
@@ -14,10 +15,22 @@ def main():
         ai_deployment_name = os.getenv('QA_DEPLOYMENT_NAME')
 
         # Create client using endpoint and key
-
+        credential = AzureKeyCredential(ai_key)
+        ai_client = QuestionAnsweringClient(endpoint=ai_endpoint, credential=credential)
 
         # Submit a question and display the answer
-
+        user_question = ''
+        while True:
+            user_question = input('\nQuestion:\n')
+            if user_question.lower() == "quit":                
+                break
+            response = ai_client.get_answers(question=user_question,
+                                            project_name=ai_project_name,
+                                            deployment_name=ai_deployment_name)
+            for candidate in response.answers:
+                print(candidate.answer)
+                print("Confidence: {}".format(candidate.confidence))
+                print("Source: {}".format(candidate.source))
 
 
     except Exception as ex:
