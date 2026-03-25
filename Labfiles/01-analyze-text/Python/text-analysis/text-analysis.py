b3718d35ec17d9d1a23913faa4e9f053abaac528
paweldywan <pawell139139@gmail.com>
2026-03-26 00:10:46 +0100

    Update .env with actual Azure endpoint and key; enhance text analysis functionality

--- Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
+++ Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
-
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 def main():
     try:
@@ -15,7 +16,8 @@ def main():
         foundry_key = os.getenv('FOUNDRY_KEY')
 
         # Create client using endpoint and key
-
+        credential = AzureKeyCredential(foundry_key)
+        ai_client = TextAnalyticsClient(endpoint=foundry_endpoint, credential=credential)
 
         # Analyze each text file in the reviews folder
         reviews_folder = 'reviews'
@@ -26,19 +28,33 @@ def main():
             print('\n' + text)
 
             # Get language
-
+            detectedLanguage = ai_client.detect_language(documents=[text])[0]
+            print('\nLanguage: {}'.format(detectedLanguage.primary_language.name))
 
             # Get sentiment
-
+            sentimentAnalysis = ai_client.analyze_sentiment(documents=[text])[0]
+            print("\nSentiment: {}".format(sentimentAnalysis.sentiment))
 
             # Get key phrases
-
+            phrases = ai_client.extract_key_phrases(documents=[text])[0].key_phrases
+            if len(phrases) > 0:
+                print("\nKey Phrases:")
+                for phrase in phrases:
+                    print('\t{}'.format(phrase))
 
             # Get entities
-
+            entities = ai_client.recognize_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nEntities")
+                for entity in entities:
+                    print('\t{} ({})'.format(entity.text, entity.category))
 
             # Get linked entities
-
+            linked_entities = ai_client.recognize_linked_entities(documents=[text])[0].entities
+            if len(linked_entities) > 0:
+                print("\nLinks")
+                for linked_entity in linked_entities:
+                    print('\t{} ({})'.format(linked_entity.name, linked_entity.url))
 
 
     except Exception as ex:
