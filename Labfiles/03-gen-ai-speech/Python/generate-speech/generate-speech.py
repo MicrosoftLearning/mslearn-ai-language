63e09fc855b00d42a5ac7db9a7c4f353f21e8076
paweldywan <pawell139139@gmail.com>
2026-03-27 22:46:05 +0100

    Enhance speech generation and transcription functionality; integrate Azure OpenAI client and improve audio processing

--- Labfiles/03-gen-ai-speech/Python/generate-speech/generate-speech.py
+++ Labfiles/03-gen-ai-speech/Python/generate-speech/generate-speech.py
@@ -4,7 +4,8 @@ from playsound3 import playsound
 from dotenv import load_dotenv
 
 # Import namespaces
-
+from openai import AzureOpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 
 
 def main():
@@ -21,12 +22,25 @@ def main():
 
 
         # Create the Azure OpenAI client
-        
+        token_provider = get_bearer_token_provider(                    
+            DefaultAzureCredential(), "https://ai.azure.com/.default"
+        )
+
+        client = AzureOpenAI(
+            azure_endpoint=endpoint,
+            azure_ad_token_provider = token_provider,
+            api_version="2025-03-01-preview"
+        )        
 
 
         # Generate speech and save to file
-        
-
+        with client.audio.speech.with_streaming_response.create(
+                    model=model_deployment,
+                    voice="alloy",
+                    input="My voice is my passport!",
+                    instructions="Speak in a serious tone.",
+                ) as response:
+            response.stream_to_file(speech_file_path)       
 
         # Play the generated speech file
         playsound(speech_file_path)
--- Labfiles/03-gen-ai-speech/Python/transcribe-speech/transcribe-speech.py
+++ Labfiles/03-gen-ai-speech/Python/transcribe-speech/transcribe-speech.py
@@ -4,8 +4,8 @@ from playsound3 import playsound
 from dotenv import load_dotenv
 
 # Import namespaces
-
-
+from openai import AzureOpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 
 def main():
     try:
@@ -23,13 +23,25 @@ def main():
         playsound(file_path)
         
         # Create the Azure OpenAI client
+        token_provider = get_bearer_token_provider(                    
+            DefaultAzureCredential(), "https://ai.azure.com/.default"
+        )
 
-
+        client = AzureOpenAI(
+            azure_endpoint=endpoint,
+            azure_ad_token_provider = token_provider,
+            api_version="2025-03-01-preview"
+        )
         
         # Call model to transcribe audio file
-
-
-
+        audio_file = open(file_path, "rb")
+        transcription = client.audio.transcriptions.create(
+            model=model_deployment,
+            file=audio_file,
+            response_format="text"
+        )
+            
+        print(transcription)
 
     except Exception as ex:
         print(ex)
