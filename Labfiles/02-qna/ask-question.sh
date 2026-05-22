a5d9d17feea2a1ca0875ea425f79e5c42ec603e9
Ivor Berry <11559977+ivorb@users.noreply.github.com>
2026-05-22 11:07:08 -0400

    remove old labs

--- Labfiles/02-qna/ask-question.sh
+++
@@ -1,6 +0,0 @@
-
-prediction_url="YOUR_ENDPOINT_URL"
-key="YOUR_KEY"
-
-curl -X POST $prediction_url -H "Ocp-Apim-Subscription-Key: $key" -H "Content-Type: application/json" -d "{'question': 'What is a learning Path?' }"
-
